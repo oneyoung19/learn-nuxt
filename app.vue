a0fa6c6d3648863afceffa7755a1b2b02d3d28d6
yangxiaoping <2878847069@qq.com>
2023-12-10 21:42:17 +0800

    feat: 基础路由

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <AppAlert></AppAlert>
+    <!-- TODO: 如果pages下创建index.vue，已经默认创建了路由。那么此处的NuxtPage的作用是什么呢？为什么下面的NuxtLink不展示呢? -->
+    <!-- <NuxtLink to="/about">About</NuxtLink> -->
+    <NuxtPage />
+    <!-- <AppAlert></AppAlert> -->
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLink to="/about">About</NuxtLink>
+  <div>This is from 'pages/index'</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
