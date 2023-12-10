c068f8472f47cea06d38552db7ebaed5a687753f
yangxiaoping <2878847069@qq.com>
2023-12-10 21:20:41 +0800

    feat: 添加app.config.ts

--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -1,11 +1,14 @@
 <template>
+  <h1>{{ appConfig.title }}</h1>
   <div>app alert</div>
 </template>
 
 <script>
 export default {
   data () {
+    const appConfig = useAppConfig()
     return {
+      appConfig
     }
   },
   methods: {
