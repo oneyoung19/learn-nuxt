4231a5e7d10a5ba8ca5fe5a0e1b0e399078b9050
yangxiaoping <2878847069@qq.com>
2023-12-10 21:18:53 +0800

    feat: components自动全局可用

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <AppAlert></AppAlert>
   </div>
 </template>
---
+++ components/AppAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>app alert</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
