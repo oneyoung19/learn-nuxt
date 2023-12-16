c0639c39bc5e24d75bdde333e607ce07fed70cdf
yangxiaoping <2878847069@qq.com>
2023-12-16 20:00:48 +0800

    feat: 抽离Quote组件

---
+++ components/ui/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="quote">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+.quote {
+  position: relative;
+  padding-left: 24px;
+  font-size: 16px;
+  color: @blank3;
+  line-height: 40px;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 4px;
+    height: 100%;
+    background-color: @blue;
+  }
+}
+</style>
