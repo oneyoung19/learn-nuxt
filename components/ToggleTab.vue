7f724ff2c1950da6ec275a4c2a2bf9ebede9d77d
yangxiaoping <2878847069@qq.com>
2023-12-19 15:58:30 +0800

    feat: 添加ToggleTab组件

---
+++ components/ToggleTab.vue
@@ -0,0 +1,97 @@
+<template>
+  <ul class="toggle-tab-list">
+    <li
+      class="toggle-tab-item"
+      :class="modelValue === item.value ? 'active' : ''"
+      v-for="(item, index) in list"
+      :key="index"
+      @click="handleClick(item)">
+      <SvgIcon class="icon" :name="modelValue === item.value ? item.activeIconName : item.iconName"></SvgIcon>
+      <span>{{ item.label }}</span>
+      <SvgIcon class="icon-arrow" name="arrow-line"></SvgIcon>
+    </li>
+    <!-- <li class="toggle-tab-item active">
+      <SvgIcon class="icon" name="wallet-light"></SvgIcon>
+      <span>全球贸易收款</span>
+      <SvgIcon class="icon-arrow" name="arrow-line"></SvgIcon>
+    </li>
+    <li class="toggle-tab-item">
+      <SvgIcon class="icon" name="bag-deep"></SvgIcon>
+      <span>电商平台收款</span>
+    </li>
+    <li class="toggle-tab-item">
+      <SvgIcon class="icon" name="cross-deep"></SvgIcon>
+      <span>国际投融资</span>
+    </li> -->
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    list: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleClick (item) {
+      const { value } = item
+      this.$emit('update:modelValue', value)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.toggle-tab-list {
+  display: inline-block;
+  .toggle-tab-item {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin-bottom: 24px;
+    padding: 0 24px;
+    width: 450px;
+    height: 90px;
+    background: @gray-gradient4;
+    font-size: 18px;
+    color: @blank2;
+    line-height: 90px;
+    border-radius: 10px;
+    cursor: pointer;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    .icon {
+      margin-right: 22px;
+      font-size: 36px;
+    }
+    .icon-arrow {
+      position: absolute;
+      top: 50%;
+      right: 38px;
+      transform: translateY(-50%);
+      display: none;
+      font-size: 26px;
+    }
+    &.active {
+      color: @topwhite;
+      background: @blank;
+      .icon-arrow {
+        display: block;
+      }
+    }
+  }
+}
+</style>
--- pages/zh-cn/corporbank.vue
+++ pages/zh-cn/corporbank.vue
@@ -8,6 +8,20 @@
         <Button type="blue">获取账户</Button>
       </div>
     </div>
+    <div class="channel">
+      <div class="channel-container layout">
+        <div class="channel-left">
+          <p class="title">贸易无国界，</p>
+          <p class="title">轻松应对每一笔收付款</p>
+          <ToggleTab
+            class="channel-list"
+            v-model="activeChannel"
+            :list="channelList">
+          </ToggleTab>
+        </div>
+        <div class="channel-right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +29,12 @@
 export default {
   data () {
     return {
+      activeChannel: 'global',
+      channelList: [
+        { label: '全球贸易收款', value: 'global', iconName: 'wallet-deep', activeIconName: 'wallet-light' },
+        { label: '电商平台收款', value: 'ecommerce', iconName: 'bag-deep', activeIconName: 'bag-light' },
+        { label: '国际投融资', value: 'internation', iconName: 'cross-deep', activeIconName: 'cross-light' }
+      ]
     }
   },
   methods: {
@@ -42,7 +62,6 @@ export default {
           font-size: 80px;
           color: @blue2;
           vertical-align: top;
-          // line-height: 80px;
         }
       }
       .desc {
@@ -51,6 +70,26 @@ export default {
         color: @gray5;
         line-height: 36px;
       }
+    }
+  }
+  .channel {
+    height: 800px;
+    background-color: @topwhite;
+    .channel-container {
+      display: flex;
+      padding-top: 120px;
+      .channel-left {
+        padding-top: 36px;
+        .title {
+          font-size: 40px;
+          color: @blank;
+          line-height: 55px;
+        }
+        .channel-list {
+          margin-top: 60px;
+        }
+      }
+      .channel-right {}
     }
   }
 }
