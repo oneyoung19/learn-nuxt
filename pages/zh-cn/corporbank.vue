7f915acbb008be5227479bb228fedbbb4f21c5f1
yangxiaoping <2878847069@qq.com>
2023-12-19 16:55:05 +0800

    feat: corporbank页面添加channel模块

--- pages/zh-cn/corporbank.vue
+++ pages/zh-cn/corporbank.vue
@@ -19,7 +19,55 @@
             :list="channelList">
           </ToggleTab>
         </div>
-        <div class="channel-right"></div>
+        <div
+          class="channel-right background"
+          :class="activeChannel">
+          <div
+            class="global-bg background"
+            v-show="activeChannel === 'global'">
+            <div class="global-img global-usd">
+              <img src="@/assets/image/corporbank/global-usd.png" alt="">
+            </div>
+            <div class="global-img global-gbp">
+              <img src="@/assets/image/corporbank/global-gbp.png" alt="">
+            </div>
+            <div class="global-img global-cad">
+              <img src="@/assets/image/corporbank/global-cad.png" alt="">
+            </div>
+            <div class="global-img global-eur">
+              <img src="@/assets/image/corporbank/global-eur.png" alt="">
+            </div>
+          </div>
+          <div
+            class="ecommerce-bg background"
+            v-show="activeChannel === 'ecommerce'">
+            <div class="ecommerce-img ecommerce-wish">
+              <img src="@/assets/image/corporbank/wish.png" alt="">
+            </div>
+            <div class="ecommerce-img ecommerce-tiktok">
+              <img src="@/assets/image/corporbank/tiktok.png" alt="">
+            </div>
+            <div class="ecommerce-img ecommerce-ebay">
+              <img src="@/assets/image/corporbank/ebay.png" alt="">
+            </div>
+            <div class="ecommerce-img ecommerce-amazon">
+              <img src="@/assets/image/corporbank/amazon.png" alt="">
+            </div>
+            <div class="ecommerce-img ecommerce-shopify">
+              <img src="@/assets/image/corporbank/shopify.png" alt="">
+            </div>
+          </div>
+          <div
+            class="internation-bg background"
+            v-show="activeChannel === 'internation'">
+            <div class="internation-amount">
+              <img src="@/assets/image/corporbank/internation-amount.png" alt="">
+            </div>
+            <div class="internation-arrow">
+              <img src="@/assets/image/corporbank/internation-arrow.png" alt="">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -77,6 +125,7 @@ export default {
     background-color: @topwhite;
     .channel-container {
       display: flex;
+      justify-content: space-between;
       padding-top: 120px;
       .channel-left {
         padding-top: 36px;
@@ -89,7 +138,102 @@ export default {
           margin-top: 60px;
         }
       }
-      .channel-right {}
+      .channel-right {
+        width: 558px;
+        height: 558px;
+        &.global {
+          background-image: url('@/assets/image/corporbank/channel-bg-01.png');
+        }
+        &.ecommerce {
+          background-image: url('@/assets/image/corporbank/channel-bg-02.png');
+        }
+        &.internation {
+          background-image: url('@/assets/image/corporbank/channel-bg-03.png');
+        }
+        .global-bg {
+          position: relative;
+          margin-top: 48px;
+          width: 100%;
+          height: 428px;
+          background-image: url('@/assets/image/corporbank/global-bg.png');
+          .global-img {
+            position: absolute;
+            width: 194px;
+            height: 110px;
+          }
+          .global-usd {
+            top: 168px;
+            left: -60px;
+          }
+          .global-gbp {
+            top: 312px;
+            left: 27px;
+          }
+          .global-cad {
+            top: 322px;
+            right: -20px;
+          }
+          .global-eur {
+            top: 178px;
+            right: -102px;
+          }
+        }
+        .ecommerce-bg {
+          position: relative;
+          margin-top: 100px;
+          width: 100%;
+          height: 358px;
+          background-image: url('@/assets/image/corporbank/ecommerce-bg.png');
+          .ecommerce-img {
+            position: absolute;
+            width: 140px;
+            height: 90px;
+          }
+          .ecommerce-wish {
+            top: -60px;
+            left: 288px;
+          }
+          .ecommerce-tiktok {
+            top: 105px;
+            right: -55px;
+          }
+          .ecommerce-ebay {
+            bottom: -70px;
+            right: 160px;
+          }
+          .ecommerce-amazon {
+            width: 234px;
+            height: 144px;
+            left: 104px;
+            bottom: -18px;
+          }
+          .ecommerce-shopify {
+            left: -10px;
+            bottom: -28px;
+          }
+        }
+        .internation-bg {
+          position: relative;
+          margin: 88px auto 0;
+          width: 380px;
+          height: 378px;
+          background-image: url('@/assets/image/corporbank/internation-bg.png');
+          .internation-amount {
+            position: absolute;
+            top: 120px;
+            left: -136px;
+            width: 264px;
+            height: 194px;
+          }
+          .internation-arrow {
+            position: absolute;
+            right: 40px;
+            bottom: 18px;
+            width: 104px;
+            height: 104px;
+          }
+        }
+      }
     }
   }
 }
