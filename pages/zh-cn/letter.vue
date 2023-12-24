b43b78f222ca9e9ec5ae7ecc60499c3cdf074c5d
yangxiaoping <2878847069@qq.com>
2023-12-24 21:08:08 +0800

    feat: 单证业务页面添加动画

--- pages/zh-cn/letter.vue
+++ pages/zh-cn/letter.vue
@@ -2,7 +2,7 @@
   <div class="letter">
     <div class="banner background">
       <div class="banner-container layout">
-        <p class="title">提供完备的银行单证服务</p>
+        <p class="title" data-aos="fade-up">提供完备的银行单证服务</p>
       </div>
     </div>
     <div class="switch">
@@ -16,148 +16,160 @@
     <div class="business" :class="activeTab">
       <div class="business-container layout">
         <ul class="business-list">
-          <li class="business-item" v-show="activeTab === 'credit'">
-            <p class="business-item-title">信用证业务（Letter of Credit）</p>
-            <div class="business-item-content">
-              <ul class="content-left">
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务介绍</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    信用证是开证行根据进口商的申请，向受益人(或出口商)出具的一种付款承诺。在受益人的交单符合信用证所规定的各项条款时，开证行即不可撤销地向受益人履行付款责任。
-                  </div>
-                  <div class="content-left-item-desc">支持具有真实贸易背景的跟单信用证。</div>
-                  <div class="content-left-item-desc">支持USD、CNY、EUR币种的信用证，其他币种请通过邮箱咨询。</div>
-                </li>
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务前提</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    受益人需是我行企业客户。
-                  </div>
-                </li>
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务咨询</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
-                  </div>
-                  <div class="content-left-item-desc tip">
-                    * 建议编辑完整信息发送至邮箱咨询：企业名称、姓名、电话、预留邮箱、具体信用证业务需求描述。
-                  </div>
-                </li>
-              </ul>
-              <div class="content-right">
-                <div class="content-right-bg">
-                  <img src="@/assets/image/letter/credit.png" alt="">
-                  <div class="credit-02">
-                    <img src="@/assets/image/letter/credit02.png" alt="">
-                  </div>
-                  <div class="credit-03">
-                    <img src="@/assets/image/letter/credit03.png" alt="">
-                  </div>
-                  <div class="credit-04">
-                    <img src="@/assets/image/letter/credit04.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="business-item" v-show="activeTab === 'documentary'">
-            <p class="business-item-title">跟单托收业务（Documentary Collections）</p>
-            <div class="business-item-content">
-              <ul class="content-left">
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务介绍</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    进口代收 Inward Bill Collection (DP):  富港银行作为代收行，接受代理行委托，按照代理行的指示向付款人提示单据，并在付款人办理了付款、承兑或者其他约定的手续以后，向其交付单据。
-                  </div>
-                  <div class="content-left-item-desc">出口托收 Outward Bill Collection (DA/DP): 富港银行作为托收行，接受出口商的委托，指示代理行向付款人提示单据，并在付款人办理了付款、承兑或者其他约定的手续以后，由代理行向其交付单据。</div>
-                  <div class="content-left-item-desc">支持USD、CNY、EUR币种的进口代收和出口托收，其他币种请通过邮箱咨询。</div>
-                </li>
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">产品特色</span>
-                  </div>
-                  <div class="content-left-item-desc">1.费用较低。相较于信用证而言，费用低，可以节约成本。</div>
-                  <div class="content-left-item-desc">2.简便易行。与信用证相比，流程简单，便捷，节省时间。</div>
-                  <div class="content-left-item-desc">3.风险较小。进口商只有在付款或承兑以后才能获得单据，与赊销方式相比，出口商承担的风险较小。</div>
-                </li>
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务咨询</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
-                  </div>
-                  <div class="content-left-item-desc tip">
-                    * 建议编辑完整信息发送至邮箱咨询：咨询时请提供托收的币种、金额、企业名称，以及对方银行名称地址等等。
-                  </div>
-                </li>
-              </ul>
-              <div class="content-right">
-                <div class="content-right-bg">
-                  <img src="@/assets/image/letter/documentary.png" alt="">
-                  <div class="documentary-02">
-                    <img src="@/assets/image/letter/documentary02.png" alt="">
-                  </div>
-                  <div class="documentary-03">
-                    <img src="@/assets/image/letter/documentary03.png" alt="">
+          <transition
+            mode="out-in"
+            name="fade">
+            <li class="business-item" v-show="activeTab === 'credit'">
+              <p class="business-item-title">信用证业务（Letter of Credit）</p>
+              <div class="business-item-content">
+                <ul class="content-left">
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务介绍</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      信用证是开证行根据进口商的申请，向受益人(或出口商)出具的一种付款承诺。在受益人的交单符合信用证所规定的各项条款时，开证行即不可撤销地向受益人履行付款责任。
+                    </div>
+                    <div class="content-left-item-desc">支持具有真实贸易背景的跟单信用证。</div>
+                    <div class="content-left-item-desc">支持USD、CNY、EUR币种的信用证，其他币种请通过邮箱咨询。</div>
+                  </li>
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务前提</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      受益人需是我行企业客户。
+                    </div>
+                  </li>
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务咨询</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
+                    </div>
+                    <div class="content-left-item-desc tip">
+                      * 建议编辑完整信息发送至邮箱咨询：企业名称、姓名、电话、预留邮箱、具体信用证业务需求描述。
+                    </div>
+                  </li>
+                </ul>
+                <div class="content-right">
+                  <div class="content-right-bg">
+                    <img src="@/assets/image/letter/credit.png" alt="">
+                    <div class="credit-02">
+                      <img src="@/assets/image/letter/credit02.png" alt="">
+                    </div>
+                    <div class="credit-03">
+                      <img src="@/assets/image/letter/credit03.png" alt="">
+                    </div>
+                    <div class="credit-04">
+                      <img src="@/assets/image/letter/credit04.png" alt="">
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li class="business-item" v-show="activeTab === 'guarantee'">
-            <p class="business-item-title">保函业务（Letter of Guarantee）</p>
-            <div class="business-item-content">
-              <ul class="content-left">
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务介绍</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    保函又称“担保”，是担保银行应申请人的要求向受益人开出的、用以保证申请人(或被担保人)履行其与受益人签定的基础合同项下义务的书面承诺。富港银行可为以我行客户为受益人的保函提供通知服务。
-                  </div>
-                  <div class="content-left-item-desc">保函的种类很多，用途十分广泛，可适用于商品、劳务、技术贸易，工程项目承包、承建，物资进出口报关，向金融机构融资，大型成套设备租赁，各种合同义务的履行等领域。</div>
-                  <div class="content-left-item-desc">常见的保函种类有，投标保函，预付款保函，履约保函，付款保函，融资保函等等。</div>
-                  <div class="content-left-item-desc">支持USD、CNY、EUR币种的保函通知，其他币种请通过邮箱咨询。</div>
-                </li>
-                <li class="content-left-item">
-                  <div class="content-left-item-header">
-                    <SvgIcon name="global"></SvgIcon>
-                    <span class="title">业务咨询</span>
-                  </div>
-                  <div class="content-left-item-desc">
-                    咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
-                  </div>
-                  <div class="content-left-item-desc tip">
-                    * 建议编辑完整信息发送至邮箱咨询：企业名称、姓名、电话、预留邮箱、具体保函业务需求描述。
-                  </div>
-                </li>
-              </ul>
-              <div class="content-right">
-                <div class="content-right-bg">
-                  <img src="@/assets/image/letter/guarantee.png" alt="">
-                  <div class="guarantee-02">
-                    <img src="@/assets/image/letter/guarantee02.png" alt="">
+            </li>
+          </transition>
+          <transition
+            mode="out-in"
+            name="fade">
+            <li class="business-item" v-show="activeTab === 'documentary'">
+              <p class="business-item-title">跟单托收业务（Documentary Collections）</p>
+              <div class="business-item-content">
+                <ul class="content-left">
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务介绍</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      进口代收 Inward Bill Collection (DP):  富港银行作为代收行，接受代理行委托，按照代理行的指示向付款人提示单据，并在付款人办理了付款、承兑或者其他约定的手续以后，向其交付单据。
+                    </div>
+                    <div class="content-left-item-desc">出口托收 Outward Bill Collection (DA/DP): 富港银行作为托收行，接受出口商的委托，指示代理行向付款人提示单据，并在付款人办理了付款、承兑或者其他约定的手续以后，由代理行向其交付单据。</div>
+                    <div class="content-left-item-desc">支持USD、CNY、EUR币种的进口代收和出口托收，其他币种请通过邮箱咨询。</div>
+                  </li>
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">产品特色</span>
+                    </div>
+                    <div class="content-left-item-desc">1.费用较低。相较于信用证而言，费用低，可以节约成本。</div>
+                    <div class="content-left-item-desc">2.简便易行。与信用证相比，流程简单，便捷，节省时间。</div>
+                    <div class="content-left-item-desc">3.风险较小。进口商只有在付款或承兑以后才能获得单据，与赊销方式相比，出口商承担的风险较小。</div>
+                  </li>
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务咨询</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
+                    </div>
+                    <div class="content-left-item-desc tip">
+                      * 建议编辑完整信息发送至邮箱咨询：咨询时请提供托收的币种、金额、企业名称，以及对方银行名称地址等等。
+                    </div>
+                  </li>
+                </ul>
+                <div class="content-right">
+                  <div class="content-right-bg">
+                    <img src="@/assets/image/letter/documentary.png" alt="">
+                    <div class="documentary-02">
+                      <img src="@/assets/image/letter/documentary02.png" alt="">
+                    </div>
+                    <div class="documentary-03">
+                      <img src="@/assets/image/letter/documentary03.png" alt="">
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </li>
+            </li>
+          </transition>
+          <transition
+            mode="out-in"
+            name="fade">
+            <li class="business-item" v-show="activeTab === 'guarantee'">
+              <p class="business-item-title">保函业务（Letter of Guarantee）</p>
+              <div class="business-item-content">
+                <ul class="content-left">
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务介绍</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      保函又称“担保”，是担保银行应申请人的要求向受益人开出的、用以保证申请人(或被担保人)履行其与受益人签定的基础合同项下义务的书面承诺。富港银行可为以我行客户为受益人的保函提供通知服务。
+                    </div>
+                    <div class="content-left-item-desc">保函的种类很多，用途十分广泛，可适用于商品、劳务、技术贸易，工程项目承包、承建，物资进出口报关，向金融机构融资，大型成套设备租赁，各种合同义务的履行等领域。</div>
+                    <div class="content-left-item-desc">常见的保函种类有，投标保函，预付款保函，履约保函，付款保函，融资保函等等。</div>
+                    <div class="content-left-item-desc">支持USD、CNY、EUR币种的保函通知，其他币种请通过邮箱咨询。</div>
+                  </li>
+                  <li class="content-left-item">
+                    <div class="content-left-item-header">
+                      <SvgIcon name="global"></SvgIcon>
+                      <span class="title">业务咨询</span>
+                    </div>
+                    <div class="content-left-item-desc">
+                      咨询邮箱：<a href="mailto:tf@cbibank.com" class="link" target="_blank">tf@cbibank.com</a>
+                    </div>
+                    <div class="content-left-item-desc tip">
+                      * 建议编辑完整信息发送至邮箱咨询：企业名称、姓名、电话、预留邮箱、具体保函业务需求描述。
+                    </div>
+                  </li>
+                </ul>
+                <div class="content-right">
+                  <div class="content-right-bg">
+                    <img src="@/assets/image/letter/guarantee.png" alt="">
+                    <div class="guarantee-02">
+                      <img src="@/assets/image/letter/guarantee02.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </transition>
         </ul>
         <div class="business-container-bg"></div>
       </div>
