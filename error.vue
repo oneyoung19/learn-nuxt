96dfedc93d13e16f8936752b5eb2789f691bee9a
yangxiaoping <2878847069@qq.com>
2023-12-28 19:12:53 +0800

    feat: 添加error页面

--- error.vue
+++ error.vue
@@ -1,14 +1,37 @@
-<script setup lang="ts">
-const props = defineProps({
-  error: Object
-})
-
-const handleError = () => clearError({ redirect: '/' })
-</script>
-
 <template>
-  <div>
+  <div class="error">
     <h2>{{ error.statusCode }}</h2>
-    <button @click="handleError">清除错误</button>
+    <Button type="blue" @click="handleError">返回首页</Button>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleError () {
+      clearError({ redirect: '/' })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.error {
+  width: 100vw;
+  height: 100vh;
+  color: @topwhite;
+  background-color: @blank;
+}
+</style>
