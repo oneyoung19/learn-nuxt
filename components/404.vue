bd3df91b1c7cf131d8be131ab00382e1e6847313
刘熠 <liuyi01@cbib.com>
2023-12-28 20:38:29 +0800

    feat: 增加error

---
+++ components/404.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="page-404">
+    <img class="gradient-404" src="@/assets/image/404/404-gradient.png" alt="" srcset="">
+    <div class="content">
+      <img src="@/assets/image/404/404-bg.png" alt="" srcset="">
+      <h1>糟糕，您访问的页面飞走了</h1>
+      <p>该页面不存在或者页面出现了错误</p>
+      <Button>返回首页</Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="less">
+.page-404 {
+  width: 100%;
+  min-height: 100%;
+  
+  .gradient-404 {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 804px;
+    height: 503px;
+  }
+
+  .content {
+    width: 860px;
+    margin: 0 auto;
+    padding: 110px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+
+    > img {
+      width: 100%;
+      height: 420px;
+      display: block;
+      margin-bottom: 80px;
+    }
+
+    > h1 {
+      font-size: 24px;
+      font-family: PingFang SC, PingFang SC;
+      font-weight: 400;
+      color: #FFFFFF;
+      margin-bottom: 30px;
+    }
+
+    > p {
+      font-size: 16px;
+      font-family: PingFang SC, PingFang SC;
+      font-weight: 400;
+      color: #FFFFFF;
+      margin-bottom: 58px;
+    }
+
+    &::before {
+      content: '';
+      width: 167px;
+      height: 167px;
+      position: absolute;
+      z-index: 2;
+      left: -320px;
+      top: 432px;
+      background: url('../assets/image/404/404-dashed.png') no-repeat;
+      background-size: 100% 100%;
+    }
+
+    &::after {
+      content: '';
+      width: 167px;
+      height: 167px;
+      position: absolute;
+      z-index: 2;
+      right: -230px;
+      top: 621px;
+      background: url('../assets/image/404/404-dashed.png') no-repeat;
+      background-size: 100% 100%;
+    }
+  }
+}
+</style>
--- error.vue
+++ error.vue
@@ -1,36 +1,33 @@
 <template>
   <div class="error">
-    <h2>{{ error.statusCode }}</h2>
-    <Button type="blue" @click="handleError">返回首页</Button>
+    <Page404 v-if="error.statusCode === 404" />
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    error: {
-      type: Object,
-      default () {
-        return {}
-      }
-    }
-  },
-  data () {
-    return {
-    }
-  },
-  methods: {
-    handleError () {
-      clearError({ redirect: '/' })
-    }
-  }
+<script setup lang="ts">
+import Page404 from '@/components/404.vue'
+
+useHead({
+  title: 'error - CBiBank',
+  meta: [
+    { name: 'keywords', content: 'cbi,CBiBank,富港银行,富港银行官网,富港,富港科技,富港技术,美国银行,离岸银行开户,境外银行开户,国际银行,出口企业收款' },
+    { name: 'description', content: 'CBiBank富港银行是一家持有美国银行牌照的美国商业银行，致力于为出口企业、进口企业、跨境电商以及出海企业和个人投资者用户提供一站式的全球金融解决方案，包括境外银行开户、跨境汇款、跨境支付、全球贸易收款等，离岸银行开户高效快捷，国际汇款安全，支持中英双语跨时区在线服务，富港银行将科技技术创新和金融创新为依托，持续推出丰富多元、适应用户需求的国际银行服务。site.' }
+  ]
+})
+
+const props = defineProps({
+  error: Object
+})
+
+function handleError () {
+  clearError({ redirect: '/' })
 }
 </script>
 
 <style scoped lang="less">
 .error {
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   color: @topwhite;
   background-color: @blank;
 }
