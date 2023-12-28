2baf4578eb92bc6bdcbb905b82b606efb0a64753
yangxiaoping <2878847069@qq.com>
2023-12-28 11:50:37 +0800

    feat: 修改CookieTip组件

--- components/business/CookieTip.vue
+++ components/business/CookieTip.vue
@@ -1,95 +1,97 @@
 <template>
   <!-- 增加cookie提示，暂时只放在了首页中，将来要放到全局提示 -->
   <div class="cookie-tip">
-    <h3>Cookie 为您提供个性化体验</h3>
-    <div class="content">
-      <p>
-        Cookie帮助我们优化您的网站使用体验，我们使用Cookie进行网站分析和个性化内容展示，
-        如果您继续访问本网站，代表您同意相关用途。更多信息，请查看我们的
-        <span @click="handlePreviewProtocol">《隐私权政策》</span>
-      </p>
+    <div class="content layout">
+      <div class="">
+        <h3>Cookie 为您提供个性化体验</h3>
+        <p>
+          Cookie帮助我们优化您的网站使用体验，我们使用Cookie进行网站分析和个性化内容展示，
+          如果您继续访问本网站，代表您同意相关用途。更多信息，请查看我们的
+          <a class="link" href="https://www.cbibank.com/privacy.html" target="__blank">《隐私权协议》</a>
+        </p>
+      </div>
       <div class="btns">
         <Button
           type="blank"
-          @click="handleAcceptCookie"
-        >
+          size="small"
+          @click="handleAcceptCookie">
           接受Cookie
         </Button>
-
-        <Button
+        <!-- <Button
           type="gray"
-          @click="handleRejectCookie"
-        >
+          size="small"
+          @click="handleRefuseCookie">
           拒绝Cookie
-        </Button>
+        </Button> -->
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+// if (!window.localStorage.getItem('cookieConsent')) {
+//   $('.cookie-consent').show()
+// }
 // 接受cookie
 function handleAcceptCookie() {
   // todo
+  // const cookieConsent = $('.cookie-consent')
+  // const height = cookieConsent.height()
+  // cookieConsent.animate({
+  //   bottom: `-${height}px`
+  // }, 500, function () {
+  //   cookieConsent.hide()
+  // })
+  // window.localStorage.setItem('cookieConsent', true)
 }
 
 // 拒绝cookie
-function handleRejectCookie() {
+function handleRefuseCookie() {
   // todo
-}
-
-function handlePreviewProtocol() {
-
 }
 </script>
 
 <style lang="less" scoped>
 .cookie-tip {
   position: fixed;
-  left: 0;
+  left: 50%;
   bottom: 0;
-  right: 0;
+  transform: translateX(-50%);
   z-index: 99;
   width: 100%;
-  height: 180px;
+  padding: 24px 60px;
   background: linear-gradient(180deg, #EAF0F7 0%, #FFFFFF 50%, #FFFFFF 100%);
   box-shadow: 0px 15px 40px 0px rgba(10,18,35,0.2);
-  border-radius: 8px 8px 8px 8px;
+  border-radius: 8px;
   border: 1px solid;
   border-image: linear-gradient(180deg, rgba(255, 255, 255, 1), rgba(187.00000405311584, 192.00000375509262, 216.00000232458115, 1)) 1 1;
-  padding: 40px 50px 36px;
-
-  h3 {
-    font-size: 20px;
-    font-family: PingFang SC, PingFang SC;
-    font-weight: 400;
-    color: #171725;
-    margin-bottom: 24px;
-  }
-
   .content {
-    width: 100%;
     display: flex;
+    justify-content: space-between;
     align-items: center;
-
-    > p {
-      flex: 1;
+    h3 {
+      font-size: 20px;
+      font-family: PingFang SC, PingFang SC;
+      font-weight: 400;
+      color: #171725;
+      margin-bottom: 16px;
+    }
+    p {
       font-size: 14px;
-      line-height: 30px;
+      line-height: 24px;
       font-family: PingFang SC, PingFang SC;
       font-weight: 400;
       color: #44444F;
-      span {
+      >.link {
         color: #4A6AFF;
         cursor: pointer;
       }
     }
-
     .btns {
       display: flex;
       justify-content: flex-end;
-      width: 400px;
       flex-shrink: 0;
+      margin-left: 130px;
     }
   }
 }
